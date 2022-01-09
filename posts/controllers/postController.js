3843eadf11f7060c7ecee631ee09d78d7f99b5ae
TsEvgeniy <evgeniy.tsvetikov@icloud.com>
2022-01-10 01:59:31 +0500

    Configure communication between Posts and Event-Bus

--- posts/controllers/postController.js
+++ posts/controllers/postController.js
@@ -16,7 +16,7 @@ exports.createPost = async (req, res) => {
     title,
   };
 
-  await axios.post('http://localhost:4005/events', {
+  await axios.post('http://event-bus-srv:4005/events', {
     type: 'PostCreated',
     data: {
       id,
